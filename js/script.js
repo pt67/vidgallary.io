bbd5087df7406ef96a8878704ccba2bdfc2dfdc7
Prakash Thapa <pt6769759@gmail.com>
2018-05-13 10:29:13 +0545

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function Selectvid1(){
+document.getElementById("myvideo").src="https://upload.wikimedia.org/wikipedia/commons/transcoded/6/61/Elephant_eating_Yala_Sri_Lanka.ogv/Elephant_eating_Yala_Sri_Lanka.ogv.480p.webm";
+}
+function Selectvid2(){
+document.getElementById("myvideo").src="https://upload.wikimedia.org/wikipedia/commons/transcoded/2/2b/Exciting_Elephant_Ride_in_Jaipur_at_Amer_Fort.webm/Exciting_Elephant_Ride_in_Jaipur_at_Amer_Fort.webm.360p.webm";
+}
+function Selectvid3(){
+document.getElementById("myvideo").src="https://upload.wikimedia.org/wikipedia/commons/transcoded/f/fa/Giraffa_camelopardalis_reticulata-atTobuZoo-2012.ogv/Giraffa_camelopardalis_reticulata-atTobuZoo-2012.ogv.480p.webm";
+}
+function Selectvid4(){
+document.getElementById("myvideo").src="https://upload.wikimedia.org/wikipedia/commons/transcoded/f/f4/Moving_Octopus_Vulgaris_2005-01-14.ogv/Moving_Octopus_Vulgaris_2005-01-14.ogv.480p.webm";
+}
